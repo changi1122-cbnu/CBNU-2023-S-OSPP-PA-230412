840d558c44175e70bd1225f11fe7157eac579e9f
Woochang Lee <changi112242@outlook.com>
2023-04-13 21:18:50 +0900

    6주차 도전09

---
+++ Week6_Challenge09/app/src/main/java/net/studio1122/week6_challenge09/MainActivity.java
@@ -0,0 +1,14 @@
+package net.studio1122.week6_challenge09;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
